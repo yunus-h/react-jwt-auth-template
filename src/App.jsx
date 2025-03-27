16de98e0140eaadd86ab683edef1a5eb590a4586
Yunus <yrherman8@gmail.com>
2025-03-27 13:58:34 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+
+import NavBar from './components/NavBar/NavBar';
+
+const App = () => {
+  
+  return (
+    <>
+      <NavBar />
+      <h1>Hello, friend!</h1>
+    </>
+  );
+};
+
+export default App;
+
+
